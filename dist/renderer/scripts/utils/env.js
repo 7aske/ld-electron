9b970b5285366119e1ca209c73314efec5bff224
Nikola Tasic <ntasic7@gmail.com>
2020-01-14 21:12:13 +0100

    rm dist

--- dist/renderer/scripts/utils/env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-window.process = process || {};
-exports.ENV = window.process.type == "renderer" ? "electron" : "web";
-exports.url = exports.ENV == "electron" ? null : "http://localhost:3000";
